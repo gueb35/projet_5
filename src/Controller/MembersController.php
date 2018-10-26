6ac2c7f897470b7c51bc3ba3079ccf453c62e918
alban <aletsy@sfr.fr>
2018-10-26 16:21:51 +0200

    Création du contenu de la page d'accueil de la partie users.

--- src/Controller/MembersController.php
+++ src/Controller/MembersController.php
@@ -10,8 +10,22 @@ class MembersController extends AbstractController
     /**
      * @Route("/members", name="members")
      */
-    public function index()
+    public function home()
     {
         return $this->render('members/homeMembers.html.twig');
+    }
+        /**
+     * @Route("/my_compte", name="my_compte")
+     */
+    public function my_compte()
+    {
+        return $this->render('members/accountMembers.html.twig');
+    }
+        /**
+     * @Route("/basket_compouned", name="basket_compouned")
+     */
+    public function basket_compouned()
+    {
+        return $this->render('members/basketCompounedMembers.html.twig');
     }
 }
