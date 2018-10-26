80a90ba0c11cdf9cd1b7ae84b7bb4dea051bf810
alban <aletsy@sfr.fr>
2018-10-26 17:41:21 +0200

    Création des vues de la partie admin.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,10 +10,36 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="home_admin")
      */
-    public function index()
+    public function homeAdmin()
     {
-        return $this->render('admin/home.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/homeAdmin.html.twig');
+    }
+    /**
+     * @Route("/prodOfWeek", name="product_of_the_week")
+     */
+    public function createListProd()
+    {
+        return $this->render('admin/prodWeek.html.twig');
+    }
+    /**
+     * @Route("/membersList", name="members_list")
+     */
+    public function membersList()
+    {
+        return $this->render('admin/membersList.html.twig');
+    }
+    /**
+     * @Route("/basketComp", name="basket_compouned_list")
+     */
+    public function showBaskCompList()
+    {
+        return $this->render('admin/basketComp.html.twig');
+    }
+    /**
+     * @Route("/basketColl", name="basket_collected")
+     */
+    public function showBaskCollList()
+    {
+        return $this->render('admin/basketColl.html.twig');
     }
 }
