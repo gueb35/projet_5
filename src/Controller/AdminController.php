de6c6c8812c772540fcc2dede7c8e2361de7afda
alban <aletsy@sfr.fr>
2018-11-14 15:25:16 +0100

    Modification des formulaires d'inscriptions aux paniers(users) et de celui definissant les produits de la semaine(admin).

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,7 +7,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Repository\ProdOfWeekRepository;
 use App\Entity\Members;
 use App\Entity\ProdOfWeek;
 
@@ -24,53 +26,159 @@ class AdminController extends AbstractController
     /**
      * @Route("/prodOfWeek", name="product_of_the_week")
      */
-    public function createListProd(Request $request, ObjectManager $manager)
+    public function createListProd(ProdOfWeekRepository $repo, Request $request, ObjectManager $manager)
     {
         $ProdOfWeeks = new ProdOfWeek();
-
-        $formProdOfWeekUnity = $this->createFormBuilder($ProdOfWeeks)
-            ->add('prodByUnity')
-            ->add('quantityProdUnity')
+/********************************** formbyunity**************************************/
+        $prod1 = $repo->findOneBy(['id' => '1']);//permet de récupérer les infos du produit identifié par l'id
+        $prod2 = $repo->findOneBy(['id' => '2']);//permet de récupérer les infos du produit identifié par l'id
+        $prod3 = $repo->findOneBy(['id' => '3']);//permet de récupérer les infos du produit identifié par l'id
+        $prod4 = $repo->findOneBy(['id' => '4']);//permet de récupérer les infos du produit identifié par l'id
+        $prod5 = $repo->findOneBy(['id' => '5']);//permet de récupérer les infos du produit identifié par l'id
+        // dump($prod1);
+        $formProdOfWeekUnity1 = $this->createFormBuilder($ProdOfWeeks)
+            ->add('prodByUnity',TextType::class)
+            ->add('quantityProdUnity',IntegerType::class)
             ->getForm();
+        $formProdOfWeekUnity2 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByUnity',TextType::class)
+        ->add('quantityProdUnity',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekUnity3 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByUnity',TextType::class)
+        ->add('quantityProdUnity',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekUnity4 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByUnity',TextType::class)
+        ->add('quantityProdUnity',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekUnity5 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByUnity',TextType::class)
+        ->add('quantityProdUnity',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekUnity1->handleRequest($request);
+        $formProdOfWeekUnity2->handleRequest($request);
+        $formProdOfWeekUnity3->handleRequest($request);
+        $formProdOfWeekUnity4->handleRequest($request);
+        $formProdOfWeekUnity5->handleRequest($request);
 
-        $formProdOfWeekUnity->handleRequest($request);
-
-        if($formProdOfWeekUnity->isSubmitted() && $formProdOfWeekUnity->isValid()) {
+        if($formProdOfWeekUnity1->isSubmitted() && $formProdOfWeekUnity1->isValid()) {
 
             $manager->persist($ProdOfWeeks);
             $manager->flush();
 
-            return $this->redirectToRoute('product_of_the_week');
+            // return $this->redirectToRoute('product_of_the_week');
         }
+        if($formProdOfWeekUnity2->isSubmitted() && $formProdOfWeekUnity2->isValid()) {
 
-        $formProdOfWeekByKg = $this->createFormBuilder($ProdOfWeeks)
-        ->add('prodByKg')
-        ->add('quantityProdKg')
+            $manager->persist($ProdOfWeeks);
+            $manager->flush();
+
+            // return $this->redirectToRoute('product_of_the_week');
+        }
+        if($formProdOfWeekUnity3->isSubmitted() && $formProdOfWeekUnity2->isValid()) {
+
+            $manager->persist($ProdOfWeeks);
+            $manager->flush();
+
+            // return $this->redirectToRoute('product_of_the_week');
+        }
+        if($formProdOfWeekUnity4->isSubmitted() && $formProdOfWeekUnity2->isValid()) {
+
+            $manager->persist($ProdOfWeeks);
+            $manager->flush();
+
+            // return $this->redirectToRoute('product_of_the_week');
+        }
+        if($formProdOfWeekUnity5->isSubmitted() && $formProdOfWeekUnity2->isValid()) {
+
+            $manager->persist($ProdOfWeeks);
+            $manager->flush();
+
+            // return $this->redirectToRoute('product_of_the_week');
+        }
+/********************************** formbykg**************************************/
+        $formProdOfWeekByKg1 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByKg',TextType::class)
+        ->add('quantityProdKg',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekByKg2 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByKg',TextType::class)
+        ->add('quantityProdKg',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekByKg3 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByKg',TextType::class)
+        ->add('quantityProdKg',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekByKg4 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByKg',TextType::class)
+        ->add('quantityProdKg',IntegerType::class)
+        ->getForm();
+        $formProdOfWeekByKg5 = $this->createFormBuilder($ProdOfWeeks)
+        ->add('prodByKg',TextType::class)
+        ->add('quantityProdKg',IntegerType::class)
         ->getForm();
 
-    $formProdOfWeekByKg->handleRequest($request);
+    $formProdOfWeekByKg1->handleRequest($request);
+    $formProdOfWeekByKg2->handleRequest($request);
+    $formProdOfWeekByKg3->handleRequest($request);
+    $formProdOfWeekByKg4->handleRequest($request);
+    $formProdOfWeekByKg5->handleRequest($request);
 
-    if($formProdOfWeekByKg->isSubmitted() && $formProdOfWeekByKg->isValid()) {
+    if($formProdOfWeekByKg1->isSubmitted() && $formProdOfWeekByKg1->isValid()) {
 
         $manager->persist($ProdOfWeeks);
         $manager->flush();
 
-        return $this->redirectToRoute('product_of_the_week');
+        // return $this->redirectToRoute('product_of_the_week');
+    }
+    if($formProdOfWeekByKg2->isSubmitted() && $formProdOfWeekByKg->isValid()) {
+
+        $manager->persist($ProdOfWeeks);
+        $manager->flush();
+
+        // return $this->redirectToRoute('product_of_the_week');
+    }
+    if($formProdOfWeekByKg3->isSubmitted() && $formProdOfWeekByKg->isValid()) {
+
+        $manager->persist($ProdOfWeeks);
+        $manager->flush();
+
+        // return $this->redirectToRoute('product_of_the_week');
+    }
+    if($formProdOfWeekByKg4->isSubmitted() && $formProdOfWeekByKg->isValid()) {
+
+        $manager->persist($ProdOfWeeks);
+        $manager->flush();
+
+        // return $this->redirectToRoute('product_of_the_week');
+    }
+    if($formProdOfWeekByKg5->isSubmitted() && $formProdOfWeekByKg->isValid()) {
+
+        $manager->persist($ProdOfWeeks);
+        $manager->flush();
+
+        // return $this->redirectToRoute('product_of_the_week');
     }
 
         return $this->render('admin/prodWeek.html.twig', [
 
-            'formProdOfWeekUnity1' => $formProdOfWeekUnity->createView(),
-            'formProdOfWeekUnity2' => $formProdOfWeekUnity->createView(),
-            'formProdOfWeekUnity3' => $formProdOfWeekUnity->createView(),
-            'formProdOfWeekUnity4' => $formProdOfWeekUnity->createView(),
-            'formProdOfWeekUnity5' => $formProdOfWeekUnity->createView(),
+            'prod1' => $prod1,
+            'prod2' => $prod2,
+            'prod3' => $prod3,
+            'prod4' => $prod4,
+            'prod5' => $prod5,
+            'formProdOfWeekUnity1' => $formProdOfWeekUnity1->createView(),
+            'formProdOfWeekUnity2' => $formProdOfWeekUnity2->createView(),
+            'formProdOfWeekUnity3' => $formProdOfWeekUnity3->createView(),
+            'formProdOfWeekUnity4' => $formProdOfWeekUnity4->createView(),
+            'formProdOfWeekUnity5' => $formProdOfWeekUnity5->createView(),
 
-            'formProdOfWeekByKg6' => $formProdOfWeekByKg->createView(),
-            'formProdOfWeekByKg7' => $formProdOfWeekByKg->createView(),
-            'formProdOfWeekByKg8' => $formProdOfWeekByKg->createView(),
-            'formProdOfWeekByKg9' => $formProdOfWeekByKg->createView(),
-            'formProdOfWeekByKg10' => $formProdOfWeekByKg->createView()
+            'formProdOfWeekByKg1' => $formProdOfWeekByKg1->createView(),
+            'formProdOfWeekByKg2' => $formProdOfWeekByKg2->createView(),
+            'formProdOfWeekByKg3' => $formProdOfWeekByKg3->createView(),
+            'formProdOfWeekByKg4' => $formProdOfWeekByKg4->createView(),
+            'formProdOfWeekByKg5' => $formProdOfWeekByKg5->createView()
         ]);
     }
 
