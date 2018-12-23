6002bb40e811735255282114add04ca6146c354b
alban <aletsy@sfr.fr>
2018-12-23 21:19:36 +0100

    Enregistrement de fichiers.Ensemble fonctionnel!

---
+++ src/Entity/MembersOfBasketCompouned.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MembersOfBasketCompounedRepository")
+ * @UniqueEntity(
+ * fields= {"email"},
+ * message= "L'email que vous avez indiqué est déjà utilisé !")
+ */
+class MembersOfBasketCompouned implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $basketType;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numberBasketRest;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $town;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $dayOfWeek;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Votre mot de passe doit faire minimum 8 caractères")
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Votre mot de passe et la confirmation de votre mot de passe doivent être identiques")
+     */
+    public $confirm_password;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\ProdBaskComp", mappedBy="members")
+     */
+    private $prodBaskComps;
+
+    public function __construct()
+    {
+        $this->prodBaskComps = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getBasketType(): ?string
+    {
+        return $this->basketType;
+    }
+
+    public function setBasketType(string $basketType): self
+    {
+        $this->basketType = $basketType;
+
+        return $this;
+    }
+
+    public function getNumberBasketRest(): ?int
+    {
+        return $this->numberBasketRest;
+    }
+
+    public function setNumberBasketRest(int $numberBasketRest): self
+    {
+        $this->numberBasketRest = $numberBasketRest;
+
+        return $this;
+    }
+
+    public function getTown(): ?string
+    {
+        return $this->town;
+    }
+
+    public function setTown(string $town): self
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    public function getDayOfWeek(): ?string
+    {
+        return $this->dayOfWeek;
+    }
+
+    public function setDayOfWeek(string $dayOfWeek): self
+    {
+        $this->dayOfWeek = $dayOfWeek;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function eraseCredentials(){}
+
+    public function getSalt(){}
+    
+    public function getRoles(){
+        return ['ROLE_USER'];
+    }
+
+    /**
+     * @return Collection|ProdBaskComp[]
+     */
+    public function getProdBaskComps(): Collection
+    {
+        return $this->prodBaskComps;
+    }
+
+    public function addProdBaskComp(ProdBaskComp $prodBaskComp): self
+    {
+        if (!$this->prodBaskComps->contains($prodBaskComp)) {
+            $this->prodBaskComps[] = $prodBaskComp;
+            $prodBaskComp->setMembers($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProdBaskComp(ProdBaskComp $prodBaskComp): self
+    {
+        if ($this->prodBaskComps->contains($prodBaskComp)) {
+            $this->prodBaskComps->removeElement($prodBaskComp);
+            // set the owning side to null (unless already changed)
+            if ($prodBaskComp->getMembers() === $this) {
+                $prodBaskComp->setMembers(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20181223133543.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181223133543 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE members_of_basket_collected (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, basket_type VARCHAR(255) NOT NULL, number_basket_rest INT NOT NULL, town VARCHAR(255) NOT NULL, day_of_week VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE members_of_basket_collected');
+    }
+}
